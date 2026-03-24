96fe02a47f5a5874f9e34fcb14caf50412aa6c40
s_baoshan <s_baoshan@163.com>
2026-03-24 10:55:24 +0800

    pages

---
+++ Pages/CheckHelper.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.Maui.Controls;
+
+namespace CheckHelper.Pages
+{
+    public partial class CheckHelper : ContentPage
+    {
+        public CheckHelper()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Pages/NetworkDebugPage.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.Maui.Controls;
+
+namespace CheckHelper.Pages
+{
+    public partial class NetworkDebugPage : ContentPage
+    {
+        public NetworkDebugPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
